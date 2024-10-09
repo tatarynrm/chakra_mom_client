f1e2f4dc77630e837d8206d3e526886f552ce9b6
tatarynrm <tatarynrm@gmail.com>
2024-10-09 23:27:02 +0300

    ewqeqw

--- src/pages/Instagram/Instagram.jsx
+++ src/pages/Instagram/Instagram.jsx
@@ -1,28 +1,27 @@
 import { Button, Stack, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import instance from '../../utils/axios';
 
 const Instagram = () => {
     const [media, setMedia] = useState([]);
-
+    const [error, setError] = useState(null);
     const handleLogin = () => {
         // window.location.href = 'http://localhost:8800/auth/instagram';
         window.location.href = 'https://api.logistic-mira.space/auth/instagram';
     };
-  
+
+    // Fetch media data
     const fetchMedia = async () => {
         try {
-            // const response = await axios.get('http://localhost:8800/manage', { withCredentials: true });
-            const response = await axios.get('https://api.logistic-mira.space/manage', { withCredentials: true });
-           
-           console.log(response.data.data);
-           
+            const response = await instance.get('/manage');
             setMedia(response.data.data);
-        } catch (error) {
-            console.error('Error fetching media:', error);
+        } catch (err) {
+            setError('Failed to fetch media');
+            console.error(err);
         }
     };
-  
+
     useEffect(() => {
         fetchMedia();
     }, []);
@@ -32,6 +31,15 @@ const Instagram = () => {
 
         <Text>INSTAGARM PAGE</Text>
         <Button onClick={handleLogin}>INSTAGRAM LOGIN</Button>
+        {error && <p>{error}</p>}
+            <ul>
+                {media.map((item) => (
+                    <li key={item.id}>
+                        <img src={item.media_url} alt={item.caption} />
+                        <p>{item.caption}</p>
+                    </li>
+                ))}
+            </ul>
     </Stack>
   )
 }
